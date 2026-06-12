bd2581194460af0eb41bb0c5e6a1021d2b3e2d31
macbook pro <macbookpro@macs-MacBook-Pro.local>
2026-06-12 13:42:51 +0100

    Render page sections on initial load

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,19 @@
-import { lazy, Suspense } from "react";
 import { LazyMotion, MotionConfig, domAnimation } from "framer-motion";
 import { useSmoothScroll } from "./hooks/useSmoothScroll";
 import { AmbientGlow } from "./components/ui/AmbientGlow";
 import { ParticleCanvas } from "./components/ui/ParticleCanvas";
 import { Navigation } from "./components/layout/Navigation";
 import { HeroSection } from "./components/sections/HeroSection";
+import { IntroSection } from "./components/sections/IntroSection";
+import { TimelineSection } from "./components/sections/TimelineSection";
+import { DoubleLifeSection } from "./components/sections/DoubleLifeSection";
+import { AuthorSection } from "./components/sections/AuthorSection";
+import { TrailerSection } from "./components/sections/TrailerSection";
+import { LifeSection } from "./components/sections/LifeSection";
+import { ReviewsSection } from "./components/sections/ReviewsSection";
+import { FeaturesSection } from "./components/sections/FeaturesSection";
+import { FinalCtaSection } from "./components/sections/FinalCtaSection";
 import { Footer } from "./components/layout/Footer";
-
-// Lazy load sections below the fold
-const IntroSection = lazy(() =>
-  import("./components/sections/IntroSection").then((m) => ({
-    default: m.IntroSection,
-  })),
-);
-const TimelineSection = lazy(() =>
-  import("./components/sections/TimelineSection").then((m) => ({
-    default: m.TimelineSection,
-  })),
-);
-const DoubleLifeSection = lazy(() =>
-  import("./components/sections/DoubleLifeSection").then((m) => ({
-    default: m.DoubleLifeSection,
-  })),
-);
-const AuthorSection = lazy(() =>
-  import("./components/sections/AuthorSection").then((m) => ({
-    default: m.AuthorSection,
-  })),
-);
-const LifeSection = lazy(() =>
-  import("./components/sections/LifeSection").then((m) => ({
-    default: m.LifeSection,
-  })),
-);
-const TrailerSection = lazy(() =>
-  import("./components/sections/TrailerSection").then((m) => ({
-    default: m.TrailerSection,
-  })),
-);
-const ReviewsSection = lazy(() =>
-  import("./components/sections/ReviewsSection").then((m) => ({
-    default: m.ReviewsSection,
-  })),
-);
-const FeaturesSection = lazy(() =>
-  import("./components/sections/FeaturesSection").then((m) => ({
-    default: m.FeaturesSection,
-  })),
-);
-const FinalCtaSection = lazy(() =>
-  import("./components/sections/FinalCtaSection").then((m) => ({
-    default: m.FinalCtaSection,
-  })),
-);
 
 function App() {
   useSmoothScroll();
@@ -67,17 +28,15 @@ function App() {
 
         <main className="relative z-[2]">
           <HeroSection />
-          <Suspense fallback={null}>
-            <IntroSection />
-            <TimelineSection />
-            <DoubleLifeSection />
-            <AuthorSection />
-            <TrailerSection />
-            <LifeSection />
-            <ReviewsSection />
-            <FeaturesSection />
-            <FinalCtaSection />
-          </Suspense>
+          <IntroSection />
+          <TimelineSection />
+          <DoubleLifeSection />
+          <AuthorSection />
+          <TrailerSection />
+          <LifeSection />
+          <ReviewsSection />
+          <FeaturesSection />
+          <FinalCtaSection />
         </main>
 
         <Footer />
